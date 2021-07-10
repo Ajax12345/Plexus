97242ef768d26d65ae1a4e020488ed64fa0dd3e7
Ajax12345 <jpetullo14@gmail.com>
2021-07-10 11:05:44 -0400

    added instructor hint message display

--- protest_v3_main.py
+++ protest_v3_main.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
-    return flask.render_template('game_window_v3_1.html')
+    return flask.render_template('game_window_v3_2.html')
 
 if __name__ == '__main__':
     app.debug = True
