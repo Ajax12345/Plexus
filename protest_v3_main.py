f28b191a85e234850a67daa22c5223ae03ab953d
Ajax12345 <jpetullo14@gmail.com>
2021-07-11 19:44:14 -0400

    added game window registry listings

--- protest_v3_main.py
+++ protest_v3_main.py
@@ -6,6 +6,22 @@ app = flask.Flask(__name__)
 def main():
     return flask.render_template('demo_landing.html')
 
+@app.route('/first-player-game-window', methods=['GET'])
+def first_player_game_window():
+    return flask.render_template('game_window_v3.html')
+
+@app.route('/second-player-game-window', methods=['GET'])
+def second_player_game_window():
+    return flask.render_template('game_window_v3_1.html')
+
+@app.route('/third-player-game-window', methods=['GET'])
+def third_player_game_window():
+    return flask.render_template('game_window_v3_2.html')
+
+@app.route('/instructor-player-game-window', methods=['GET'])
+def instructor_player_game_window():
+    return flask.render_template('game_window_v3_3.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
