797bc1a6977c2249b1535f482eb4a689955a6e45
Ajax12345 <jpetullo14@gmail.com>
2021-07-12 15:21:11 -0400

    added progress bar

--- static/js/create_game.js
+++ static/js/create_game.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-
+    function format_progress_bar(){
+        for (var i of document.querySelectorAll('.step-listing-col')){
+            if (!$(i).data('circle')){
+                $(`#step-progress-col${$(i).data('sid')} > .progress-bar`).css('height', $(i).css('height'));
+            }
+            else{
+                var nh = (parseInt($(i).css('height').match('\\d+')) - parseInt($(`#step-progress-col${$(i).data('sid')} > .process-circle`).css('height').match('\\d+')))/2;
+                $(`<div class='progress-bar' style='height:${nh}px'></div>`).insertBefore(`#step-progress-col${$(i).data('sid')} > .process-circle`)
+                $(`<div class='progress-bar' style='height:${nh}px'></div>`).insertAfter(`#step-progress-col${$(i).data('sid')} > .process-circle`)
+                
+            }
+        }
+    }
+    /*
+    setTimeout(function(){
+        format_progress_bar();
+    }, 500)
+    ^will need this
+    */
+    format_progress_bar();
 });
