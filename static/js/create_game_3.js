5833225fdc42d05913b0248770b537cfbc7a2321
Ajax12345 <jpetullo14@gmail.com>
2021-07-14 13:55:18 -0400

    game matrix selection added

--- static/js/create_game_3.js
+++ static/js/create_game_3.js
@@ -12,8 +12,32 @@ $(document).ready(function(){
             }
         }
     }
+    function display_content_cards(){
+        for (var i of document.querySelectorAll('.content-card')){
+            var _h = parseInt($(i).css('height').match('\\d+'));
+            var _desc_path = `#content-card${$(i).data('card')} > .content-description`
+            if (_h < 170){
+                $(`<div style='height:${170-_h}px'></div>`).insertAfter(_desc_path);
+            }
+            else if (_h > 170){
+                var cid = $(i).data('card');
+                var _desc = $(_desc_path).text();
+                var last_d = null;
+                for (var i = 0; i < _desc.length; i++){
+                    var t_ld = _desc.substring(0, i)+'...';
+                    $(_desc_path).html(`${t_ld}(<span class='see-more-desc'>see more</span>)`);
+                    if (parseInt($(`#content-card${cid}`).css('height').match('\\d+')) > 170){
+                        break;
+                    }
+                    last_d = t_ld;
+                }
+
+            }
+        }
+    }
     setTimeout(function(){
         format_progress_bar();
+        display_content_cards();
     }, 100)
     $('body').on('click', '.step-progress-button', function(){
         window.location.replace($(this).data('tolink'))
