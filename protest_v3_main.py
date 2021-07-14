443f683114d1d0532884503cd6b6a548d38d7810
Ajax12345 <jpetullo14@gmail.com>
2021-07-14 13:58:29 -0400

    cache disabling

--- protest_v3_main.py
+++ protest_v3_main.py
@@ -34,6 +34,21 @@ def third_player_game_window():
 def instructor_player_game_window():
     return flask.render_template('game_window_v3_3.html')
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    r.headers.add('Access-Control-Allow-Origin', '*')
+    r.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    r.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return r
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
