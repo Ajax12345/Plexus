9dbed1b6024c366ff8421bc038657b3f2917b45b
Ajax12345 <jpetullo14@gmail.com>
2021-07-17 19:52:48 -0400

    added basic content card display on dashboard

--- static/js/dashboard_content.js
+++ static/js/dashboard_content.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-
+    //<span class="see-more-desc">see more</span>
+    function adjust_content_card_heights(){
+        for (var i of document.querySelectorAll('.content-card')){
+            var c_path = `#content-description${$(i).data('card')}`;
+            if (parseInt($(c_path).css('height').match('\\d+')) > 150){
+                var c = null;
+                var content = $(c_path).text();
+                for (var j = 0; j < content.length; j++){
+                    $(c_path).html(`${content.substring(0, j)}...<span class="see-more-desc">see more</span>`)
+                    if (parseInt($(c_path).css('height').match('\\d+')) > 150){
+                        break;
+                    }
+                    c = j;
+                }
+                $(c_path).html(`${content.substring(0, c)}...<span class="see-more-desc">see more</span>`)
+            }
+        }
+    }
+    setTimeout(function(){
+        adjust_content_card_heights();
+    }, 300);
 });
