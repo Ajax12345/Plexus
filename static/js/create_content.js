56a0f6fb3e231b417b332db30b298f49e4d7e737
Ajax12345 <jpetullo14@gmail.com>
2021-07-18 18:39:21 -0400

    updates to content creation

--- static/js/create_content.js
+++ static/js/create_content.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    //pass
+    function format_progress_bar(){
+        for (var i of document.querySelectorAll('.step-listing-col')){
+            if (!$(i).data('circle')){
+                $(`#step-progress-col${$(i).data('sid')} > .progress-bar`).css('height', $(i).css('height'));
+            }
+            else{
+                var nh = (parseInt($(i).css('height').match('\\d+')) - parseInt($(`#step-progress-col${$(i).data('sid')} > .process-circle`).css('height').match('\\d+')))/2;
+                $(`<div class='progress-bar' style='height:${nh}px'></div>`).insertBefore(`#step-progress-col${$(i).data('sid')} > .process-circle`)
+                $(`<div class='progress-bar' style='height:${nh}px'></div>`).insertAfter(`#step-progress-col${$(i).data('sid')} > .process-circle`)
+                
+            }
+        }
+    }
+    //$('.game-name-field').focus();
+    setTimeout(function(){
+        format_progress_bar();
+    }, 100)
 });
