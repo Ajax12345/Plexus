54df80982646d997e9195cb7135b1f001b319181
Ajax12345 <jpetullo14@gmail.com>
2021-09-04 20:59:46 -0400

    updates

--- static/js/sign_up.js
+++ static/js/sign_up.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-
+    $('body').on('input', '.entry-field', function(){
+        if (Array.from(document.querySelectorAll('.entry-field')).every(function(x){return $(x).val().length > 0})){
+            $('.sign-up').removeClass('sign-up-disabled')
+        }
+        else{
+            $('.sign-up').addClass('sign-up-disabled')
+        }
+    });
+    $('body').on('click', '.sign-up', function(){
+        if (!$(this).hasClass('sign-up-disabled')){
+            
+        }
+    });
 });
