222f0511a04cdf899971b52a20c54689c7664a91
Ajax12345 <jpetullo14@gmail.com>
2021-09-14 21:02:48 -0400

    start of game creation

--- static/js/create_game.js
+++ static/js/create_game.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var step = 1;
+    var game_payload = {name:null, rounds:6, content:null, matrix:null};
     function format_progress_bar(){
         for (var i of document.querySelectorAll('.step-listing-col')){
             if (!$(i).data('circle')){
@@ -15,13 +17,25 @@ $(document).ready(function(){
     $('.game-name-field').focus();
     setTimeout(function(){
         format_progress_bar();
-    }, 100)
-    $('body').on('click', '.step-progress-button', function(){
-        window.location.replace($(this).data('tolink'))
+    }, 100);
+    $('body').on('input', '.game-name-field', function(){
+        game_payload.name = $(this).val();
     });
     $('body').on('input', '.round-number-field', function(){
-        if (parseInt($(this).val()) < 1){
+        var r = parseInt($(this).val())
+        if (r < 1){
             $(this).val('1')
+            game_payload.rounds = r;
+
         }
+    });
+    function step_1(){
+        if (game_payload.name === null || game_payload.name.length === 0){
+            
+        }
+    }
+    var step_handlers = {1:step_1};
+    $('body').on('click', '.next-step-button', function(){
+        step_handlers[step]()
     });
 });
