72aac5dd766556a5b248d3dfb0263e8d588e9d05
Ajax12345 <jpetullo14@gmail.com>
2021-09-22 19:54:25 -0400

    updates

--- static/js/game_window_v3.js
+++ static/js/game_window_v3.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $('body').on('click', '.game-content-close-top', function(){
         $('.game-content-modal').css('display', 'none');
     });
+    $('body').on('click', '.close-content-modal', function(){
+        $('.game-content-modal').css('display', 'none');
+    });
 });
