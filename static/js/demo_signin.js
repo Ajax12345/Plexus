882d03dd7c467fb41229e1a8e3af9c59837c6a52
Ajax12345 <jpetullo14@gmail.com>
2021-09-22 21:48:35 -0400

    updates

--- static/js/demo_signin.js
+++ static/js/demo_signin.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-    alert("in here")
+    $('body').on('input', '.access-demo-field', function(){
+        $(this).removeClass('access-demo-field-error')
+    }); 
+    function start_demo(){
+        var _payload = {}
+        for (var i of document.querySelectorAll('.access-demo-field')){
+            if ($(i).val().length === 0){
+                $(i).addClass('access-demo-field-error');
+                return;
+            }
+            _payload[$(i).data('field')] = $(i).val();
+        }
+    }
+    $('body').on('click', '.start-demo', function(){
+        start_demo();
+    });
 });
