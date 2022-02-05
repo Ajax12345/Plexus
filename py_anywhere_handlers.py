7df8595cb673e10e83fa4d05798f42c586a774fb
Ajax12345 <jpetullo14@gmail.com>
2022-02-05 12:26:17 -0500

    file to store pyanywhere handlers

---
+++ py_anywhere_handlers.py
@@ -0,0 +1,5 @@
+with protest_db.DbClient(host='Ajax1234.mysql.pythonanywhere-services.com', user='Ajax1234', password='Gobronxbombers2', database='Ajax1234$protest_db', as_dict = True) as cl:
+    pass
+
+with open('/home/Ajax1234/protestV3/round_response_templates.json') as f, open('/home/Ajax1234/protestV3/demo_1_about_slide.json') as f2:
+    pass
