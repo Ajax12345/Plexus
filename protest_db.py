d5acf6654d58308c618a4a3205ab8ace60b98e0e
James Petullo <jpetullo14@gmail.com>
2024-12-05 18:56:07 -0500

    Update protest_db.py

--- protest_db.py
+++ protest_db.py
@@ -10,7 +10,12 @@ class DbClient:
         conv = conversions.copy()
         conv[FIELD_TYPE.DECIMAL] = float
         conv[FIELD_TYPE.NEWDECIMAL] = float
-        self.connection = pymysql.connect(host=self.host, user=self.user, password=self.password, database=self.database, conv = conv, cursorclass=pymysql.cursors.SSCursor if not self.__dict__.get('as_dict', False) else pymysql.cursors.DictCursor)
+        self.connection = pymysql.connect(host=self.host, 
+                    user=self.user, 
+                    password=self.password, 
+                    database=self.database, 
+                    conv = conv, 
+                    cursorclass=pymysql.cursors.SSCursor if not self.__dict__.get('as_dict', False) else pymysql.cursors.DictCursor)
         self.cursor = self.connection.cursor()
         return self
 
@@ -28,6 +33,6 @@ class DbClient:
         self.connection.close()
 
 if __name__ == '__main__':
-    with DbClient(host='localhost', user='root', password='Gobronxbombers2', database='protest_db') as cl:
+    with DbClient(host='localhost', user='root', password='*******', database='protest_db') as cl:
         cl.execute('select now()')
         print(cl.fetchone())
